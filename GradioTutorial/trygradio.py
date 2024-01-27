ca8a5457b5310f9288ec90c22f99d9629d51cfa4
Drosrin <drosrin@qq.com>
2024-01-27 17:29:37 +0800

    1. Bug fixed. 2. Add Basic AI Now fine-tunes are required, and we need to modify 'answer', add other relics. Also, we need to improve AI performance.

--- GradioTutorial/trygradio.py
+++ GradioTutorial/trygradio.py
@@ -1,5 +1,17 @@
 import gradio as gr
 from PIL import Image
+import zhipuai
+
+name_refer = {
+    "a": "马踏飞燕",
+    "b": "TestNameB",
+    "c": "TestNameC",
+    "d": "TestNameD"
+}
+
+zhipuai.api_key = "c075c10b0d98a821e49af2ba4b5b1424.xk4AlAwO9oUiOEyw"
+
+history = []
 
 title="博物馆小助手"
 
@@ -7,7 +19,7 @@ description = "点击按钮，向AI提关于该文物的问题"
 
 situation = ''
 
-def greet(relic: str) -> function:
+def greet(relic: str):
     """The function should return a function, in which the image and introduction to the relic will be provided.
     
     Parameters:
@@ -19,36 +31,48 @@ def greet(relic: str) -> function:
     ----------
     A function. The function will return a tuple in format (image, introduction).
     """
-    #print(relic)
     try:
         image = Image.open(f"GradioTutorial/Artifacts/{relic}/pic.jpg")
         intro = open(f"GradioTutorial/Artifacts/{relic}/intro.txt", "r").read()
     except:
-        print(f"Failed to open File with argument relic = {relic}.")
+        print(f"Failed to open File with argument relic_id = {relic}.")
         image = None
         intro = ""
     global situation
     situation = relic
+    
     def func():
+        global history
+        prompt = f"""
+        你是一个人工智能助手。现在，你将被提供一份关于{name_refer[relic]}的简介。
+        {intro}
+        请根据接下来的提问，结合这一份简介作出回答。
+        """
+        history = [
+            {"role":"user", "content": prompt}
+        ] # Clear the original history, ready for a new relic.
+        # Note that this is not finished, and should be done with a find-tuned prompt.
         return image, intro
     return func
 
-def ai(question):
+def ai(question: str) -> str:
     """
     A todo function, will be used to interact with AI,
     """
-    question = list(question)
-    #for i in question:
-     #   if i == '时':
-      #      word= 1
-    if situation== 'a':
-        return "ss"
-    if situation== 'b':
-        return 'ee'
-    if situation=='c':
-        return 'ev'
-    if situation=='d':
-        return 'dd'
+    history.append(
+        {"role":"user", "content":question}
+    )
+    response = zhipuai.model_api.invoke(
+        model = "glm-3-turbo",
+        prompt = history,
+        temperature = 0.,
+    )
+    answer = response['data']['choices'][0]['content']
+    history.append(
+        {"role":"AI", "content":answer}
+    )
+    print(history)
+    return str(answer)# It seems that 'answer' needs a modification
 
 def clear_input():
     """
@@ -64,11 +88,12 @@ def clear_input():
     """
     return "",""
 
+########################  MAIN  ########################
 with gr.Blocks() as demo:
     gr.Markdown("# 博物馆小助手")
     gr.Markdown("点击按钮，向AI提关于该文物的问题")
     with gr.Row():
-        a=gr.Button("马踏飞燕")
+        a=gr.Button("a")
         b=gr.Button("b")
         c=gr.Button("c")
         d=gr.Button("d")
@@ -90,3 +115,4 @@ with gr.Blocks() as demo:
     c.click(fn=greet('c'),inputs=[],outputs=[imagew,introduction])
     d.click(fn=greet('d'),inputs=[],outputs=[imagew,introduction])
 demo.launch()
+########################  MAIN  ########################
