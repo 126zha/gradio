4eff18f2c6b76bf948d9e8b42e1d3867fab20b00
Drosrin <drosrin@qq.com>
2024-01-29 16:13:06 +0800

    Rewrite Question&Answer with Chatbot Module. Find a Bug.

--- GradioTutorial/trygradio.py
+++ GradioTutorial/trygradio.py
@@ -44,10 +44,9 @@ def greet(relic: str):
     def func():
         global history
         prompt = f"""
-        你是一个人工智能助手。现在，你将被提供一份关于{name_refer[relic]}的简介。
-        {intro}
-        请根据接下来的提问，结合这一份简介作出回答。
-        """
+你是一个人工智能助手。现在，你将被提供一份关于{name_refer[relic]}的简介。
+{intro}
+请根据接下来的提问，结合这一份简介作出回答。"""
         history = [
             {"role":"user", "content": prompt}
         ] # Clear the original history, ready for a new relic.
@@ -55,9 +54,19 @@ def greet(relic: str):
         return image, intro
     return func
 
-def ai(question: str) -> str:
+def LLM_Reply(question: str) -> str:
     """
-    A todo function, will be used to interact with AI,
+    Main AI Function, provided to return the answer to the user question.
+
+    Parameters:
+    ----------
+    question : str
+        An arbitrary string, provided by user, and is thus not controllable.
+    
+    Return value:
+    ----------
+    answer : str
+        A string, produced by ZhipuAI API (https://maas.aminer.cn/pricing). This should be the answer to the input.
     """
     history.append(
         {"role":"user", "content":question}
@@ -65,14 +74,21 @@ def ai(question: str) -> str:
     response = zhipuai.model_api.invoke(
         model = "glm-3-turbo",
         prompt = history,
-        temperature = 0.,
+        temperature = .05,
     )
-    answer = response['data']['choices'][0]['content']
+    answer = ""
+    if (response["success"]):
+        answer = response['data']['choices'][0]['content']
+    else:
+        answer = response['msg']
     history.append(
-        {"role":"AI", "content":answer}
+        {"role":"assistant", "content":answer}
     )
+    #""" ADD # TO DEBUG, DEL TO UNDO
+    print(answer)
     print(history)
-    return str(answer)# It seems that 'answer' needs a modification
+    #"""
+    return answer # It seems that 'answer' needs a modification
 
 def clear_input():
     """
@@ -90,29 +106,39 @@ def clear_input():
 
 ########################  MAIN  ########################
 with gr.Blocks() as demo:
-    gr.Markdown("# 博物馆小助手")
+    gr.Markdown("#博物馆小助手")
     gr.Markdown("点击按钮，向AI提关于该文物的问题")
     with gr.Row():
-        a=gr.Button("a")
-        b=gr.Button("b")
-        c=gr.Button("c")
-        d=gr.Button("d")
+        Button_A=gr.Button(name_refer["a"])
+        Button_B=gr.Button(name_refer["b"])
+        Button_C=gr.Button(name_refer["c"])
+        Button_D=gr.Button(name_refer["d"])
     imagew = gr.Image()
     introduction = gr.Textbox(label="introduction")
     with gr.Column():
-        question=gr.Textbox(label="question")
-        answer= gr.Textbox(label="answer")
+        chatbot = gr.Chatbot()
+        question=gr.Textbox(label = "问题",
+                            placeholder = "如果您有问题，可以在此进一步输入！",
+                            interactive = True)
+        
+        def respond(message, chat_history):
+            bot_message = LLM_Reply(message)
+            chat_history.append((message, bot_message))
+            return "", chat_history
+        
+        question.submit(respond, [question, chatbot], [question, chatbot])
     with gr.Row():
-        submit=gr.Button("submit")
-        clear=gr.Button("clear")
-    
-    
-    
-    submit.click(fn=ai,inputs=question,outputs=answer)
-    clear.click(fn=clear_input,inputs=[],outputs=[question,answer])
-    a.click(fn=greet('a'),inputs=[],outputs=[imagew,introduction])
-    b.click(fn=greet('b'),inputs=[],outputs=[imagew,introduction])
-    c.click(fn=greet('c'),inputs=[],outputs=[imagew,introduction])
-    d.click(fn=greet('d'),inputs=[],outputs=[imagew,introduction])
-demo.launch()
+        sbmt = gr.Button(value = "提交",
+                         fn = respond,
+                         inputs = [question, chatbot],
+                         outputs = [question, chatbot]) # abreviation for 'SUBMIT', in case of conflict
+                                                        # Buggy, cannot correctly submit the question
+        clear = gr.ClearButton([chatbot, question])
+    Button_A.click(fn=greet('a'),inputs=[],outputs=[imagew,introduction])
+    Button_B.click(fn=greet('b'),inputs=[],outputs=[imagew,introduction])
+    Button_C.click(fn=greet('c'),inputs=[],outputs=[imagew,introduction])
+    Button_D.click(fn=greet('d'),inputs=[],outputs=[imagew,introduction])
+
+if __name__ == "__main__":
+    demo.launch()
 ########################  MAIN  ########################
